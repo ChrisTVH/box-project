641170436720c04d046e9c711e2b62a8de47242a
Christopher Herasme <5632734-christvh@users.noreply.gitlab.com>
2026-09-17 19:26:50 -0400

    chore(release): align box-rpg and box-rpg-maker at 26.9.64

--- box-gui/src/box_gui/__init__.py
+++ box-gui/src/box_gui/__init__.py
@@ -2,6 +2,6 @@
 
 from __future__ import annotations
 
-__version__ = "26.9.62"
+__version__ = "26.9.64"
 
 __all__ = ["__version__"]
--- box-rpg/src/box/__init__.py
+++ box-rpg/src/box/__init__.py
@@ -1,3 +1,3 @@
 """box-rpg package."""
 
-__version__ = "26.9.62"
+__version__ = "26.9.64"
